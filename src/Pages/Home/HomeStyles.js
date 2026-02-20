40317f9f0de9e32f87b4219ffcda7d502e48c7d1
asimgope1 <asimgope53@gmail.com>
2026-02-20 15:52:23 +0530

    all new with some ui chnages

--- src/Pages/Home/HomeStyles.js
+++ src/Pages/Home/HomeStyles.js
@@ -57,7 +57,7 @@ export const styles = StyleSheet.create({
     bottom: 50,
     right: spacing.sm,
     padding: spacing.md,
-    backgroundColor: colors.surface,
+    backgroundColor: 'transparent',
     borderRadius: radius.sm,
     ...shadows.sm,
   },
